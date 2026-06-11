ae58ca5655da7d52ba0ef5739f2ae1e01147ab6d
rajnish <rajnish.kumar@hostbooks.com>
2026-06-11 16:29:34 +0530

    cors bean added

--- taskManagement/src/main/java/com/assignments/taskManagement/config/SecurityConfig.java
+++ taskManagement/src/main/java/com/assignments/taskManagement/config/SecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -54,6 +59,39 @@ public class SecurityConfig {
 
         return provider;
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration =
+                new CorsConfiguration();
+
+        configuration.setAllowedOrigins(
+                List.of("http://localhost:4200"));
+
+        configuration.setAllowedMethods(
+                List.of(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "OPTIONS"
+                ));
+
+        configuration.setAllowedHeaders(
+                List.of("*"));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration(
+                "/**",
+                configuration);
+
+        return source;
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http) throws Exception {
@@ -77,7 +115,7 @@ public class SecurityConfig {
                         .anyRequest()
                         .authenticated()
                 )
-
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session
 
                         .maximumSessions(1)
